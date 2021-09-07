900ec263a1c276f87a299c1a8c9c813efadc1f42
mad-skull <da.madskull@gmail.com>
2021-09-07 21:16:07 +0530

    kotlin-android-extensions

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun onDigit(view: android.view.View) {}
+    fun onDigit(view: android.view.View) {
+        tvInput.append
+    }
 }

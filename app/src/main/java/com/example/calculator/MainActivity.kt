b8ed54d76d72630296490fdb9c173761e1e0d96d
mad-skull <da.madskull@gmail.com>
2021-09-07 22:30:02 +0530

    adding all operations

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -65,6 +65,36 @@ class MainActivity : AppCompatActivity() {
                     }
 
                     tvInput.text = (one.toDouble() - two.toDouble()).toString()
+                }else if(tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = (one.toDouble() + two.toDouble()).toString()
+                }else if(tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = (one.toDouble() * two.toDouble()).toString()
+                }else if(tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = (one.toDouble() / two.toDouble()).toString()
                 }
 
 
